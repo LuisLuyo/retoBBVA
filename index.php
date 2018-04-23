002c26ced712307fa50d207d58be466e02e4196e
LuisLuyo <luis.luyohernandez@gmail.com>
2018-04-23 15:30:33 -0500

    Iniciando Proyecto retoBBVA, Creando carpetas iniciales, Importando Framework Slim

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        
+        <script type="text/javascript" src="lib/jquery/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="lib/jquery/jquery-ui.js"></script>
+        <script type="text/javascript" src="lib/jquery/mask/jquery-mask.js"></script>
+        
+        <script type="text/javascript" src="lib/sweetalert/js/sweetalert.min.js"></script>
+        
+        <script type="text/javascript" src="lib/bootstrap/js/bootstrap.js"></script>
+        <script type="text/javascript" src="lib/bootstrap/js/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="lib/bootstrap/locales/bootstrap-datepicker.es.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">   
+        <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap-datepicker.css">
+            
+            <link rel="stylesheet" type="text/css" href="lib/sweetalert/css/sweetalert.css">
+
+        <link rel="stylesheet" type="text/css" href="src/css/Estilos.css">   
+
+        <script type="text/javascript" src="src/js/principal.js"></script>
+    </head>
+    <div class="container" style="margin:auto">
+        <div class="card card-container" id="card" style=""></div>
+    </div>
+    <script type="text/javascript">
+        $(function(){
+            Principal();
+        });
+    </script>
+</html>
